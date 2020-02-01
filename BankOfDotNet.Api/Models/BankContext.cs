988256e9abf301f05d5b2bad567cdb414f6392b4
Edin Jašarević <edinjasar14@gmail.com>
2020-02-01 03:12:23 +0100

    add the database context

---
+++ BankOfDotNet.Api/Models/BankContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace BankOfDotNet.Api.Models
+{
+    public class BankContext : DbContext
+    {
+        public BankContext(DbContextOptions<DbContext> options) : base(options)
+        { 
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
