d1f54b46941cacfbbed88f0868a3a6a0280854d3
Edin Jašarević <edinjasar14@gmail.com>
2020-02-02 14:35:37 +0100

    consume identity server

--- BankOfDotNet.ConsoleClient/Program.cs
+++ BankOfDotNet.ConsoleClient/Program.cs
@@ -1,12 +1,39 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using IdentityModel.Client;
 
 namespace BankOfDotNet.ConsoleClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) => MainAsync().GetAwaiter().GetResult();
+
+        [Obsolete]
+        private static async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+            //dicover all the endpoints using metadata od identity server
+            var disco = await IdentityModel.Client.DiscoveryClient.GetAsync("http://localhost:50000");
+            if (disco.IsError)
+            {
+                Console.WriteLine(disco.Error);
+                return;
+            }
+
+            //grab a bearer token
+            var tokenClient = new TokenClient(disco.TokenEndpoint, "client", "secret");
+            var tokenResponse = await tokenClient.RequestClientCredentialsAsync("bankOfDotNetApi");
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+
+
         }
     }
 }
