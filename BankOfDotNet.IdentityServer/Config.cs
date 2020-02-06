5521c5f7393799e6269852372962ba391391f07f
Edin Jašarević <edinjasar14@gmail.com>
2020-02-06 21:04:18 +0100

    add resource owners

--- BankOfDotNet.IdentityServer/Config.cs
+++ BankOfDotNet.IdentityServer/Config.cs
@@ -3,11 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using IdentityServer4.Models;
+using IdentityServer4.Test;
 
 namespace BankOfDotNet.IdentityServer
 {
     public class Config
     {
+        public static List<TestUser> GetUsers()
+        {
+            return new List<TestUser>
+            {
+                new TestUser
+                {
+                    SubjectId = "1",
+                    Username = "Manish",
+                    Password = "password"
+                },
+                new TestUser
+                {
+                    SubjectId = "2",
+                    Username = "Bob",
+                    Password = "password"
+                }
+            };
+        }
+
         public static IEnumerable<ApiResource> GetAllApiResources()
         {
             return new List<ApiResource>
