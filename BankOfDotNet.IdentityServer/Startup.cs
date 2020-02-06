ebe08013a387acf2c94277f2a6dd964d0211bc10
Edin Jašarević <edinjasar14@gmail.com>
2020-02-06 21:06:16 +0100

    configure resource owners

--- BankOfDotNet.IdentityServer/Startup.cs
+++ BankOfDotNet.IdentityServer/Startup.cs
@@ -19,7 +19,8 @@ namespace BankOfDotNet.IdentityServer
             services.AddIdentityServer()
                 .AddDeveloperSigningCredential()
                 .AddInMemoryApiResources(Config.GetAllApiResources())
-                .AddInMemoryClients(Config.GetClients());
+                .AddInMemoryClients(Config.GetClients())
+                .AddTestUsers(Config.GetUsers());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
